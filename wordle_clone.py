13c4b2a51b180b5f0fbe1f6f6aed94988c2efc5b
Karolina Gustaitis <rakolina@gmail.com>
2022-05-23 13:12:29 +0300

    documented the program better

--- wordle_clone.py
+++ wordle_clone.py
@@ -1,6 +1,19 @@
-# import nltk;
-# nltk.download('popular')
-# from nltk.corpus import words
+
+# select a random five letter word that is not a title (a name)
+# ask user for a guess
+# colorize the guess and display it
+#   when letter is a hit in correcdt position colorize green
+#   when letter is a hit in a wrong position colorize orange
+#   when letter is a miss colorize red
+#
+# score keeping:
+#    
+# return to user for next guess
+
+
+
+
+
 import random
 from nltk.corpus import words
 from nltk.tag import pos_tag
@@ -14,54 +27,64 @@ class color:
    
 
 wordlist = words.words()
-# print("all words: ", len(wordlist))
+alphabet = ['q','w','e','r','t','y','u','i','o','p',
+            'a','s','d','f','g','h','j','k','l',
+            'z','x','c','v','b','n','m']
+guesses = []
 
-def colorize_letter(alphabet, letter, c):
-    for l in range(len(alphabet)):
-        if alphabet[l] == letter:
-            alphabet[l] = c + alphabet[l] + color.END
-            
-    
-alphabet = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
-def print_alphabet(score):
-    row1 = 10
-    row2 = 19
-    counter = 0
-    for letter in alphabet:
-        counter+=1
-        print (letter, end=' ')
-        if counter == row1 or counter == row2:
-            print()
-            
-        
-counter=0
-random_word = random.choice(wordlist)
-while (len(random_word) !=5 or random_word.istitle()):
-    counter+=1
-    random_word = random.choice(wordlist)
+def print_alphabet (guesses):
+    for (guess, score) in guesses:
+        for s in range (len (score)):
+            if s == '0':
+                
 
-random_word_letters = list(random_word)
-#print (random_word_letters)
 
-score = ['0']
-while '0' in score or '1' in score:
+def print_guess (guesses):
+    (guess_word, score) = guesses [-1]
+    guess_word_letters = list (guess_word)
+    for i in range (5):
+        if score[i] == 0:
+            print (color.RED + guess_word_letters[i] + color.END, end = ' ')
+        if score[i] == 1:
+            print (color.YELLOW + guess_word_letters[i] + color.END, end = ' ')
+        if score[i] == 2:
+            print (color.GREEN + guess_word_letters[i] + color.END, end = ' ')
+    print()
+
+       
+counter = 0
+random_word = random.choice (wordlist)
+while (len (random_word) !=5 or random_word.istitle()):
+    counter += 1
+    random_word = random.choice (wordlist)
+
+random_word_letters = list (random_word)
+print (random_word_letters)
+
+score = [0,0,0,0,0]
+while 0 in score or 1 in score:
     guess_word = input("Guess a five letter word: ")
-    while (len(guess_word) !=5 or guess_word not in wordlist):
+    if guess_word == '':
+        print ("The word was: ", random_word_letters)
+        print ("Bye")
+        exit()
+    
+    while (len (guess_word) !=5 or guess_word not in wordlist):
         guess_word = input("Try again: ")
     
     guess_word_letters = list(guess_word)
-    score = []
+    score = [0, 0, 0, 0, 0]
     for i in range(5):
         if guess_word_letters[i] == random_word_letters[i]:
-            colorize_letter(alphabet, guess_word_letters[i], color.GREEN)
+            score[i] = 2
         elif guess_word_letters[i] in random_word_letters:
-            colorize_letter(alphabet, guess_word_letters[i], color.YELLOW)
+            score[i] = 1
         else:
-            if guess_word_letters[i] in alphabet:
-                colorize_letter(alphabet, guess_word_letters[i], color.RED)
-
-    print (guess_word_letters)
-    print_alphabet(alphabet)
+            score[i] = 0
+    guess = (guess_word, score)
+    guesses.append (guess)
+    print_guess (guesses)
+    print_alphabet (guesses)
 
 #system('clear')
 
